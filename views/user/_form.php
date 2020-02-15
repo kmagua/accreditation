2292a4510eb6e082a088a76b7632597931c3c371
samuelmayenga <sammy.mayenga@gmail.com>
2020-02-15 18:51:57 +0300

    Merge branch 'master' of https://github.com/kmagua/accreditation
    # Conflicts:
    #	views/company-profile/_view.php

--- views/user/_form.php
+++ views/user/_form.php
@@ -8,6 +8,25 @@ use yii\widgets\ActiveForm;
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
+<div class="container" style="margin-top:2%;background-color:white;margin-left:-5%" id="Log">
+    <div class="col-md-5 col-md-offset-3 alert alert-info" style="background-color:white;border:solid 1px  #009933;-moz-border-radius:8px;-webkit-border-radius:8px;border-radius:8px;">
+        <div style="text-align:center;"><img src="../../web/images/icta.png" height="60"; width="100" /></div>
+        <br />
+    
+        <div class="row">
+            <h3 class="text-center" style="background-color: red;width:100%;color:white;height:8%;text-align:center;margin-top:-15.5px;
+            font-family:'Times New Roman', 'Times', Arial, sans-serif;font-weight:100">
+                ICT Authority Accreditation System
+            </h3>
+        </div>
+        <br>
+        <div class="row">
+            <h4 class="text-center" style=width:100%;color:green;height:6%;text-align:center;margin-top:-15.5px;text-justify: 
+            font-family:'Times New Roman', 'Times', Arial, sans-serif;font-weight:100">
+                Register Account
+            </h4>
+        </div>
+        <br />
 <div class="user-form">
 
     <?php $form = ActiveForm::begin(); ?>
@@ -23,9 +42,16 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'password_repeat')->passwordInput(['maxlength' => true]) ?>
 
     <div class="form-group">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+        <div class="col-md-6">
+       <?= Html::submitButton('Register', ['class' => 'btn btn-success']) ?>
+        </div>
+        <div class="col-md-5 col-md-offset-1">
+            <?= Html::a('Back to Login', ['/site/login'], ['class'=>'btn btn-default']) ?>
+        </div>
     </div>
 
     <?php ActiveForm::end(); ?>
 
+</div>
+    </div>
 </div>
