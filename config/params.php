f5a0ce58e2691ac6a7f34f152b8ab640b2dbf732
Kenneth Magua <kenmagua@gmail.com>
2020-10-14 21:10:56 +0300

    ignored all config settings

--- config/params.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'adminEmail' => 'admin@example.com',
-    'senderEmail' => 'noreply@example.com',
-    'senderName' => 'Example.com mailer',
-    'secAssigner' => ['charles.waithiru@ict.go.ke', 'charles.waithiru@icta.go.ke', 'kenmagua@gmail.com'],
-    'committeeAssigner' => ['james.wafula@ict.go.ke', 'james.wafula@icta.go.ke', 'kenmagua@gmail.com']
-];
